2d391ba6bdcc29ec9507481e1c1a3abd8d5bd585
babak35102 <babak35102@gmail.com>
2019-09-14 15:42:41 +0430

    raw project

---
+++ IndividualInfo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IndividualInfo.MvcApplication" Language="C#" %>
---
+++ IndividualInfo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IndividualInfo.Startup))]
+namespace IndividualInfo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
