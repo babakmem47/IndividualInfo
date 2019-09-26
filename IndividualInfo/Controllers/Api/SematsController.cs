9078893593e965bfd32d6026c63fbdb403dc2d2e
babak35102 <babak35102@gmail.com>
2019-09-26 18:11:44 +0330

    Add Semats Api Controller to return list of semats + Initialize Modal form element when editing

---
+++ IndividualInfo/Controllers/Api/SematsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IndividualInfo.Dtos;
+using IndividualInfo.Models;
+
+namespace IndividualInfo.Controllers.Api
+{
+    public class SematsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public SematsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public IHttpActionResult GetSemats()
+        {
+            var semats = _context.Semats.ToList();
+
+            var sematDto = semats.Select(s => new SematDto()
+            {
+                Id = s.Id,
+                Name = s.Name
+            });
+
+            return Ok(sematDto);
+        }
+    }
+}
