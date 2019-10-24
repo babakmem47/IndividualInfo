1b130da2ada339a85d723971cdbd205cdc1fa236
babak35102 <babak35102@gmail.com>
2019-10-24 16:30:03 +0330

    Router Controller + View

---
+++ IndividualInfo/Controllers/Api/RoutersController.cs
@@ -0,0 +1,16 @@
+﻿using IndividualInfo.Models;
+using System.Web.Http;
+
+namespace IndividualInfo.Controllers.Api
+{
+    public class RoutersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public RoutersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+    }
+}
